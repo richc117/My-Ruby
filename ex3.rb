8e318165e5cd94efc6452c4a9d55a484a91039c1
Richard Caballero <richc117@gmail.com>
2011-12-12 13:50:28 -0800

    exercise 3

---
+++ ex3.rb
@@ -0,0 +1,26 @@
+puts "i will now count my chickens:"
+# This line adds 25 + 5 and prints the result 30. The following line 
+# multiples 25 by 3 and mods it by 4 then subtracts this from 100 = 97
+puts "Hens", 25 + 30 / 6
+puts "roosters", 100 - 25 * 3 % 4
+#Adds, suntracts, divides, and also mods a bunch of stuff
+puts "Now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# Asks whether 5 is less than -2  outputs false
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7 
+# does adition and subtraction
+puts "What is 3 + 2?", 3 + 2
+puts "What is 5 - 7?", 5 - 7
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+# Operations output true or false
+puts "Is it greater?", 5 > -2
+puts "Is it greater or equal?", 5 >= -2
+puts "Is it less or equal?", 5<= -2
+puts "5 to the power 3", 5 ** 3
+

88c5db530c4d50297472b5de7f81231de014998f
Richard Caballero <richc117@0-1f-5b-ca-92-5a.dynamic.ucsd.edu>
2011-12-12 12:56:09 -0800

    initioal commit

---
+++ ex1.rb
@@ -0,0 +1,14 @@
+puts "Hello World!"
+puts "Hello again"
+puts "I like typing this."
+puts "This is fun."
+puts 'Yay! Printing.'
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+puts "I should really finish that email."
+# "i should really finish the email"
+puts # "its just make things go grey."
+puts "the gray code wont work"
+# This will be ignored by everyone else but , but i will be able to see this.
+puts "the gray code does work and i see it!"
+

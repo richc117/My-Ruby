71b5edc7378732526e6009f4f830789963950ac1
Richard Caballero <richc117@gmail.com>
2011-12-16 20:48:05 -0800

    ex7.rb

---
+++ ex7.rb
@@ -0,0 +1,33 @@
+# Prints out string of text "Mary had a little lamb"
+puts "Mary had a little lamb."
+# Prints out string of text with another string in the string "Snow"
+puts "Its fleece was white as %s." % 'snow'
+# Prints out string of text "And everywhere that Mary went."
+puts "And everywhere that Mary went."
+# Prints out ten "." and does so by using the math notation "*" to multiply
+puts "." * 10 
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "b"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# notice how we are using print instead of puts here. chaonge it to puts 
+# and see what happens.
+print end1 + end2 + end3 + end4 + end5 + end6 
+puts end7 + end8 + end9 + end10 + end11 + end12
+puts "." * 10 
+puts end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
+puts "." * 10
+print end1 + end2 + end8 + end10 + " " + end7 + end3 + end4 + end12 + "!!!"
+# this just is polite use of the terminal, try removing it 
+puts "lol"

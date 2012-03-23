33d250fdee747a378486fdf687bd997aff95ecea
Richard Caballero <richc117@gmail.com>
2012-03-22 17:52:10 -0700

    added ex 16 and 17

--- ex15.rb
+++ ex15.rb
@@ -1,15 +1,21 @@
+#This variable holds the arguments you pass to your Ruby script
+#when you run it.
 filename = ARGV.first
-
-prompt = ">"
+#variable prompt used whenever there is user inpu, prints.
+prompt = "> "
+#Function that opens filename, and assigns it to variable txt.
 txt = File.open(filename)
-
+#Prints the content of ex15_sample.txt, read command for txt.
 puts "Here's your file: #{filename}"
-puts txt.read()
+print txt.read()
+print "\n"
+
 
 puts "I'll also ask you to type it again:"
 print prompt
+#read the users input and assign to file_name
 file_again = STDIN.gets.chomp()
-
+#Function command to open file assigned to variable file_again. 
 txt_again = File.open(file_again)
 
 puts txt_again.read()
